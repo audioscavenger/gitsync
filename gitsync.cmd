985ae7a71001b39661d6d233e66ba49f12c28170
Laurent Derewonko <lderewonko@nqzw.com>
2022-12-17 17:12:19 -0700

    WIP 2.0.14 - git config http.postBuffer 209715200 	modified:   backup/gitsync.7z 	modified:   gitsync.cmd

--- gitsync.cmd
+++ gitsync.cmd
@@ -2,7 +2,7 @@
 pushd %~dp0
 
 :top
-set version=2.0.13
+set version=2.0.14
 set author=audioscavenger
 
 :defaults
@@ -41,6 +41,7 @@ call "%~dpn0.cfg.cmd" >NUL 2>&1
 IF "%~d0"=="%PROJECT%\" echo ERROR: this cannot work at the root drive without a PROJECT name & timeout /t 5 & exit 1
 :: redirect origin: git remote set-url origin https://gitea.derewonko.com/audioscavenger/nQpupeteer
 git config core.autocrlf true
+git config http.postBuffer 209715200
 call :setup_time
 call :set_colors
 :: WIP: notepad++ unfortunately does not work with start /wait
